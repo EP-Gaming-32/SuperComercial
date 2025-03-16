a1c465596477fe7d4473840f47f80074481a6ef4
EP-Gaming-32 <eduardo.a.piovezan@gmail.com>
2025-03-16 05:04:27 -0300

    tela cadastro e login

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,95 +1,31 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Head from "next/head";
+import styles from "./login.module.css";
 
-export default function Home() {
+export default function Login() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+    <Head>
+      <meta charset="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Login</title>
+    </Head>
+    <div className={styles.container}>
+        <div className={styles.loginBox}>
+            <div className={styles.header}>Login</div>
+            <div className={styles.avatar}>
+                <span>👤</span>
+            </div>
+            <form className={styles.form} action="cadastro/">
+                <label htmlfor="login" className={styles.label}>Login</label>
+                <input type="text" id="login" placeholder="Login/Email" className={styles.input}/>
+            
+                <label for="senha" className={styles.label}>Senha <a href="#">Esqueceu a Senha?</a></label>
+                <input type="password" id="senha" placeholder="Senha" className={styles.input}/>
+            
+                <button className={styles.button} type="submit">Login</button>
+            </form>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+    </>
+  )
 }
