0c03445f15952610b51c7d4c6e0190220a35c466
EP-Gaming-32 <eduardo.a.piovezan@gmail.com>
2025-04-21 20:53:53 -0300

    backend - endpoint put - produtos

--- backend/controllers/produtos.js
+++ backend/controllers/produtos.js
@@ -37,3 +37,97 @@ export const listarProdutos = async(req, res) =>{//funcao para retornar os produ
     }
 
 };
+
+//funcao para registrar produto no banco
+export const criarProduto = async(req, res) =>{
+
+    //recebe os campos do front
+    const { sku, nome_produto, id_grupo, valor_produto, prazo_validade, unidade_medida, codigo_barras} = req.body;
+
+    //checa campos obrigatorios
+    if (!sku || !nome_produto || !valor_produto) {
+        return req.status(400).json({message: "Campos Obrigatórios Faltando"})
+    }
+
+    try {
+        //query para inserir o entry no banco
+        const [result] = await pool.query(
+            `INSERT INTO Produtos
+            (sku, nome_produto, id_grupo, valor_produto, prazo_validade, unidade_medida, codigo_barras)
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            //array para passar os valores do request para o query no lugar dos ?
+            [sku, nome_produto, id_grupo || null, valor_produto, prazo_validade || null, unidade_medida || null, codigo_barras || null]
+        );
+
+        //estrutura da resposta
+        res.status(201).json({
+            message: "Produto Registrado Com Sucesso!", id_produto: result.insertId
+        });
+
+    } catch(error){
+        console.error("Erro em criar produto", error);
+
+        if (error.code === 'ER_DUP_ENTRY'){
+            return res.status(409).json({error: "SKU Duplicado"})
+        }
+        res.status(500).json({error: "erro interno criar produto"})
+    }
+};
+
+//funcao para atualizar produto
+export const atualizarProduto = async(req, res) =>{
+
+    //pega o id do produto a ser atualizado
+    const {id} = req.params;
+    //pega os valores do produto
+    const dados = req.body;
+
+    //define quais campos podem ser alterados
+    const camposPermitidos = [
+        'sku',
+        'nome_produto',
+        'id_grupo',
+        'valor_produto',
+        'prazo_validade',
+        'unidade_medida',
+        'codigo_barras'
+    ];
+
+    //variavel para armazenar os campos e valores para atualizar
+    const fields = [];
+    const values = [];
+
+    //checa se ha alterações nos campos permitidos e controi o query dinamicamente
+    for (const campo of camposPermitidos){
+        if (dados[campo] !== undefined){
+            fields.push(`${campo} = ?`);
+            values.push(dados[campo]);
+        }
+    }
+
+    //caso n haja alteração, retorna 
+    if (fields.length === 0){
+        return res.json({
+            message: "Nenhuma alteração feita"
+        })
+    }
+
+    try{
+        //query dinamico com os campos alterados
+        const [result] = await pool.query(
+            `UPDATE Produtos SET ${fields.join(',')} WHERE id_produto = ?`,
+            [...values, id]
+        );
+        
+        //chega se o entry existe, caso não retorna erro
+        if (result.affectedRows === 0){
+            return res.status(404).json({message: "Produto nao encontrado!"})
+        }
+
+        //resposta.json
+        res.json({message: "Produto atualizado!"})
+    } catch(error){
+        console.error('Erro ao atualizar produto:', error);
+        res.status(500).json({ message: 'Erro interno ao atualizar produto' });
+    }
+};
--- backend/routes/produtos.js
+++ backend/routes/produtos.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { listarProdutos } from '../controllers/produtos.js';
+import { listarProdutos, criarProduto, atualizarProduto } from '../controllers/produtos.js';
 
 const router = Router();
 
 //endpoint GET /produtos?page=<n>&limit=<m>
 router.get('/', listarProdutos);
+//endpoint POST /produtos
+router.post('/', criarProduto);
+//endpoint PUT /produtos/:id
+router.put('/:id', atualizarProduto);
 
 export default router;
